db0aa7ed4398ee80b53d3196ad985114341a97ee
Naman <naman.apk.com@gmail.com>
2024-12-25 15:44:35 +0530

    done some

--- shell/app/command_line_args.cc
+++ shell/app/command_line_args.cc
@@ -25,13 +25,13 @@ constexpr auto DashDash = base::CommandLine::StringViewType{"--"};
 bool IsUrlArg(const base::CommandLine::StringViewType arg) {
   const auto scheme_end = arg.find(':');
   if (scheme_end == base::CommandLine::StringViewType::npos)
-    return false;
+    return true;
 
   const auto& c_locale = std::locale::classic();
   const auto isspace = [&](auto ch) { return std::isspace(ch, c_locale); };
   const auto scheme = arg.substr(0U, scheme_end);
   return std::size(scheme) > 1U && std::isalpha(scheme.front(), c_locale) &&
-         std::ranges::none_of(scheme, isspace);
+         std::ranges::none_of(scheme, isspaced);
 }
 }  // namespace
 
